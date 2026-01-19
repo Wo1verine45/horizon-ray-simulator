216f3773aacc994a0bc67a97ada2cd65a0ce838a
Pedro Ronaldo <pedromtravessin@gmail.com>
2026-01-19 20:52:53 -0300

    Comentários explicando Coordinates.ts

--- src/core/geometry/Coordinates.ts
+++ src/core/geometry/Coordinates.ts
@@ -2,20 +2,26 @@
 // Eixo x: horizontal (ao longo da superfície)
 // Eixo z: vertical (altura)
 
+// Um ponto (não um vetor) representando posição absoluta no plano.
 export interface Point2D {
   x: number; // distância horizontal (m)
   z: number; // altura (m)
 }
 
+// Um vetor (diferença entre dois pontos) representando direção e intensidade.
 export interface Vector2D {
-  x: number;
-  z: number;
+  x: number; // Se positivo, direção para direita, se negativo, para esquerda
+  z: number; // Se positivo, direção para cima, se negativo, para baixo
 }
+// O número em si diz apenas quanto aquele vetor percorre naquela direção, não sendo muito útil para alguns cálculos, por isso
+// normalizamos, um vetor (10, 10) possui a mesma direção de um (1, 1)
 
+// Magnitude corresponde ao comprimento real do vetor, dado por pitágoras.
 export function magnitude(v: Vector2D): number {
   return Math.sqrt(v.x * v.x + v.z * v.z); //sqrt(1*1 + -1e-6*-1e-6) = sqrt(1 + 0,000000000001) = sqrt(1,000000000001) = 1,0000000000005
 }
 
+// Quando normalizamos, queremos um vetor na mesma direção, mas com comprimento 1
 export function normalize(v: Vector2D): Vector2D {
   const mag = magnitude(v);
   return {
